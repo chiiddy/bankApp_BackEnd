e5efc18c9fa825b1c1cf165b9f584e1efa5e0a2a
chidicolly11@gmail.com <chidicolly11@gmail.com>
2023-09-17 20:58:15 +0100

    backEnd backApps

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://magneto-banking-applications.onrender.com",
   })
 );
 
